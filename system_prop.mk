4dfa28eb6b474e88b10cb15b30187bccbc6ecddd
3055313848 <35842285+3055313848@users.noreply.github.com>
2020-11-30 20:36:46 +0800

    Update system_prop.mk
    volte

--- system_prop.mk
+++ system_prop.mk
@@ -47,7 +47,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.radio.multisim.config=ssss \
     persist.rmnet.data.enable=true \
     persist.vendor.radio.5g_mode_pref=1 \
-    telephony.lteOnCdmaDevice=1
+    telephony.lteOnCdmaDevice=1 \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
